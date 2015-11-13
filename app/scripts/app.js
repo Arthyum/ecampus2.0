0e687a2d088ef0894f9aaac53c9a48347cb1a612
Alex Deneuvillers <alex.deneuvillers@gmail.com>
2015-11-13 17:29:46 +0100

    Add Angular project

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ecampus20App
+ * @description
+ * # ecampus20App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ecampus20App', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecampus20App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ecampus20App
+ */
+angular.module('ecampus20App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
